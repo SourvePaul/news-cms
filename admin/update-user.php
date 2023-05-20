2bc5917ed5535fd244a42dabfae3c60c8d1a2e05
SourvePaul <sourvepaul99@gmail.com>
2023-05-21 01:14:05 +0600

    t2

--- admin/update-user.php
+++ admin/update-user.php
@@ -1,4 +1,23 @@
-<?php include "header.php"; ?>
+<?php include "header.php"; 
+
+if(isset($_POST['submit'])) {
+    include "config.php";
+
+    $userid = mysqli_real_escape_string($conn, $_POST['user_id']);
+    $fname = mysqli_real_escape_string($conn, $_POST['f_name']);
+    $lname =mysqli_real_escape_string($conn, $_POST['l_name']);
+    $user = mysqli_real_escape_string($conn, $_POST['username']);
+    //$password = mysqli_real_escape_string($conn, md5($_POST['password']));
+    $role = mysqli_real_escape_string($conn, $_POST['role']);
+
+    $sql = "UPDATE user SET first_name = '{$fname}', last_name = '{$lname}', username = '{$user}', role = '{$role}' WHERE user_id = {$userid}";
+
+            if(mysqli_query($conn, $sql)) {
+                header("Location: {$hostname}/admin/users.php");
+            }
+    }
+
+?>
 <div id="admin-content">
     <div class="container">
         <div class="row">
