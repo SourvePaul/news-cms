dc73b0187cc2684d32ab7469c1e6267828736c0c
SourvePaul <sourvepaul99@gmail.com>
2023-05-22 03:10:21 +0600

    t8

--- admin/header.php
+++ admin/header.php
@@ -37,7 +37,10 @@ if(!isset($_SESSION["username"])) {
                 <!-- /LOGO -->
                 <!-- LOGO-Out -->
                 <div class="col-md-offset-9  col-md-1">
-                    <a href="logout.php" class="admin-logout">logout</a>
+                    <a href="logout.php" class="admin-logout">
+                        <?php echo $_SESSION['username'];?>
+                        logout
+                    </a>
                 </div>
                 <!-- /LOGO-Out -->
             </div>
